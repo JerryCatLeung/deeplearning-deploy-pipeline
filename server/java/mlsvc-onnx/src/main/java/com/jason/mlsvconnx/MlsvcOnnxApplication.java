1275c2d0588ba81861e91a86744cd749387cd43e
jason <zhao.zhong@outlook.com>
2023-10-10 08:29:17 +0000

    feat: add onnx-runntime java maven example

---
+++ server/java/mlsvc-onnx/src/main/java/com/jason/mlsvconnx/MlsvcOnnxApplication.java
@@ -0,0 +1,13 @@
+package com.jason.mlsvconnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlsvcOnnxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlsvcOnnxApplication.class, args);
+	}
+
+}
---
+++ server/java/mlsvc-onnx/src/test/java/com/jason/mlsvconnx/MlsvcOnnxApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jason.mlsvconnx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class MlsvcOnnxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+    void test1() {
+        System.out.println(111);
+    }
+
+}
